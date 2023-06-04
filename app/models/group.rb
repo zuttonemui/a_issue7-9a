17f040681424386d8c4fa2a51408e608d5f202d2
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-06-04 04:26:27 +0000

    [Finish]8c

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :group_users
-  has_many :user, through: :group_users
+  has_many :users, through: :group_users
   
   validates :name, presence: true
   validates :introduction, length:{ maximum: 100 }
